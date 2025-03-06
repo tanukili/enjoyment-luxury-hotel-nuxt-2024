f6713fc564d47cd590e60b3925d1ecf704078aa6
tanukile <backpacker320@gmail.com>
2025-03-07 05:48:19 +0800

    fix: 500 imageUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,7 +47,7 @@ export default defineNuxtConfig({
         },
         {
           property: 'og:image',
-          content: 'https://enjoyment-luxury-hotel-nuxt-2024.onrender.com/home-hero-sm.png'
+          content: '/home-hero-sm.png'
         },
         {
           property: 'og:url',
@@ -68,7 +68,7 @@ export default defineNuxtConfig({
         }
       ],
       link: [
-        { rel: 'icon', href: 'https://enjoyment-luxury-hotel-nuxt-2024.onrender.com/logo-primary.svg' },
+        { rel: 'icon', href: '/logo-primary.svg' },
         { rel: 'canonical', href: 'https://enjoyment-luxury-hotel-nuxt-2024.onrender.com/' }
       ],
       // 結構化資料
@@ -80,7 +80,7 @@ export default defineNuxtConfig({
             "@type": "WebSite",
             "name": "享樂酒店",
             "url": "https://enjoyment-luxury-hotel-nuxt-2024.onrender.com/",
-            "image": "https://enjoyment-luxury-hotel-nuxt-2024.onrender.com/home-hero-sm.png",
+            "image": "/home-hero-sm.png",
             "description": "享樂酒店坐落於高雄市中心，是頂級奢華酒店的首選。我們為旅客提供高品質的住宿體驗，結合現代時尚設計與五星級設施，讓您盡享舒適與尊榮服務。立即預訂，體驗無與倫比的豪華住宿！（本網站為虛擬訂房網，純粹作為前端技術練習用）"
           })
         }
